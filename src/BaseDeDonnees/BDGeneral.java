344239ae1cd5dec3802b40c83f29d97791cfd395
Nathan FAURE <nathan.faure@etu.univ-orleans.fr>
2017-06-15 14:11:54 +0200

    Modification de la fonction idQuestionMax

--- src/BaseDeDonnees/BDGeneral.java
+++ src/BaseDeDonnees/BDGeneral.java
@@ -1,81 +1,3 @@
-<<<<<<< HEAD
-package BaseDeDonnees;
-
-import java.sql.*;
-import java.util.ArrayList;
-
-public class BDGeneral {
-
-  BDConnexionMySQL connexion;
-  Statement st;
-  
-  public BDGeneral (BDConnexionMySQL c) {
-		this.connexion = c;
-		try {
-	        this.st = this.connexion.mysql.createStatement();
-	    }
-
-		catch (SQLException e) {
-			
-		}
-	}
-
-  // 'C' pour les concepteurs
-  // 'S' pour les sondeurs
-  // 'A' pour les analystes
-  public ArrayList <Questionnaire> getListeQuestionnaire (char etat) {
-	  ArrayList <Questionnaire> listeQu = new ArrayList <Questionnaire> ();
-	  try {
-		  String requete = "SELECT * FROM QUESTIONNAIRE WHERE Etat = '"+etat+"';";
-		  ResultSet rs = this.st.executeQuery(requete);
-		  while (rs.next()) {
-			  Questionnaire q = new Questionnaire (rs.getInt("idQ"), rs.getString("Titre"), rs.getString("Etat").charAt(0), rs.getInt("numC"), rs.getInt("idU"), rs.getInt("idPan"));
-			  listeQu.add(q);
-	
-		  }
-		  rs.close();
-		  return listeQu;
-	  }
-
-	  catch (SQLException e) {
-		  return null;
-	  }
-  }
-	
-  public ArrayList <Question> getListeQuestion (int idQuestionnaire) {
-	  ArrayList <Question> listeQuestions = new ArrayList <Question> ();
-	  try {
-		  String requete = "SELECT * FROM QUESTION WHERE idQ = "+idQuestionnaire+";";
-		  ResultSet rs = this.st.executeQuery(requete);
-		  while (rs.next()) {
-			  Question q = new Question (rs.getInt("idQ"), rs.getInt("numQ"), rs.getString("texteQ"), rs.getInt("MaxVal"), rs.getString("idT").charAt(0));
-			  listeQuestions.add(q);
-		  }
-		  rs.close();
-		  return listeQuestions;
-	  }
-	  
-	  catch (SQLException e) {
-		  return listeQuestions;
-	  }
-  }
-  
-  public Client getClientDuQuestionnaireX (int idQuestionnaire) {
-      try {
-        String requete = "SELECT * FROM CLIENT WHERE numC = (SELECT numC FROM QUESTIONNAIRE WHERE idQ = "+idQuestionnaire+");";
-        ResultSet rs = this.st.executeQuery(requete);
-        rs.next();
-        return new Client (rs.getInt("numC"), rs.getString("raisonSoc"), rs.getString("adresse1"), rs.getString("adresse2") , rs.getInt("CodePostal"), rs.getString("Ville"), rs.getString("Telephone"), rs.getString("email"));
-      }
-
-      catch (SQLException e) {
-        return null;
-      }
-  }
-
-  
-
-=======
 package BaseDeDonnees;
 
 import java.sql.*;
@@ -194,8 +116,5 @@ public class BDGeneral {
 	  }
   }
   
-  
-  
 
->>>>>>> 38699a40ed781a87b41066a8522b96a08f677744
 }
--- src/BaseDeDonnees/BDModuleConcepteur.java
+++ src/BaseDeDonnees/BDModuleConcepteur.java
@@ -1,169 +1,4 @@
-<<<<<<< HEAD
-package BaseDeDonnees;
 
-import java.sql.*;
-
-public class BDModuleConcepteur {
-	
-	BDConnexionMySQL connexion;
-	Statement st;
-	
-	public BDModuleConcepteur (BDConnexionMySQL c) {
-		this.connexion = c;
-		
-		try {
-	        this.st = this.connexion.mysql.createStatement();
-	    }
-		
-		catch (SQLException e) {	
-		
-		}
-	}
-	
-	
-	
-	
-	
-	public int maxIdentifiantQuestionnaire () {
-		try {
-	        String requete = "SELECT MAX(idQ) idMax FROM QUESTIONNAIRE;";
-	        ResultSet rs = this.st.executeQuery(requete);
-	        rs.next();
-	        return rs.getInt("idMax");
-	      }
-		
-		catch (SQLException e) {
-	         return -1;
-	      }
-	}
-	
-	public void insererQuestionnaire (Questionnaire q) {
-		try {
-			String requete = "INSERT INTO QUESTIONNAIRE (idQ, Titre, Etat, numC, idU, idPan) VALUES (?,?,?,?,?,?);";
-			PreparedStatement ps = this.connexion.mysql.prepareStatement(requete);
-			ps.setInt(1, q.getNumeroQuestionnaire());
-			ps.setString(2, q.getTitreQuestionnaire());
-			ps.setString(3, String.valueOf(q.getEtatQuestionnaire()));
-			ps.setInt(4, q.getNumClient());
-			ps.setInt(5, q.getIdentifiantUtilisateur());
-			ps.setInt(6, q.getIdentifiantPanel());
-			ps.executeUpdate();
-		}
-
-		catch (SQLException e) {
-      
-		}
-	}
-	
-	// A REFAIRE
-	public void supprimerQuestionnaire (int identifiantQuestionnaire) {
-		try {
-	        String requete = "DELETE FROM QUESTIONNAIRE WHERE idQ = "+identifiantQuestionnaire+";";
-	        this.st.executeUpdate(requete);
-		}
-		
-		catch (SQLException e) {
-	        
-	    }
-	}
-	
-	
-	
-	
-	
-	public int maxIdentifiantClient () {
-		try {
-	        String requete = "SELECT MAX(numC) idMax FROM CLIENT;";
-	        ResultSet rs = this.st.executeQuery(requete);
-	        rs.next();
-	        return rs.getInt("idMax");
-	      }
-		
-		catch (SQLException e) {
-	         return -1;
-	      }
-	}
-	
-	public void insererClient (Client c) {
-		try {
-			String requete = "INSERT INTO CLIENT (numC, raisonSoc, adresse1, adresse2, CodePostal, Ville, Telephone, email) VALUES (?,?,?,?,?,?,?,?);";
-			PreparedStatement ps = this.connexion.mysql.prepareStatement(requete);
-			ps.setInt(1, c.getNumeroClient());
-			ps.setString(2, c.getRaisonSociale());
-			ps.setString(3, c.getAdresse1());
-			ps.setString(4, c.getAdresse2());
-			ps.setInt(5, c.getCodePostal());
-			ps.setString(6, c.getVille());
-			ps.setString(7, c.getNumeroTelephone());
-			ps.setString(8, c.getEmail());
-			ps.executeUpdate();
-		}
-		
-		catch (SQLException e) {
-			
-		}
-	}
-
-	
-	
-	
-	
-	public int maxIdentifiantQuestion () {
-		try {
-	        String requete = "SELECT MAX(numQ) idMax FROM QUESTION;";
-	        ResultSet rs = this.st.executeQuery(requete);
-	        rs.next();
-	        return rs.getInt("idMax");
-	      }
-		
-		catch (SQLException e) {
-	         return -1;
-	      }
-	}
-	
-	public void insererQuestion (Question q) {
-		try {
-			String requete = "INSERT INTO QUESTION (idQ, numQ, texteQ, MaxVal, idT) VALUES (?,?,?,?,?);";
-			PreparedStatement ps = this.connexion.mysql.prepareStatement(requete);
-			ps.setInt(1, q.getIdQuestionnaire());
-			ps.setInt(2, q.getNumeroQuestion());
-			ps.setString(3, q.getTexteQuestion());
-			ps.setInt(4, q.getMaxValeur());
-			ps.setString(5, String.valueOf(q.getIdTypeQuestion()));
-			ps.executeUpdate();
-		}
-
-		catch (SQLException e) {
-
-		}
-	}
-
-	public void supprimerQuestion (int idQuestionnaire, int numeroQuestion) {
-		try {
-	        String requete = "DELETE FROM QUESTION WHERE idQ = "+idQuestionnaire+" AND numQ = "+numeroQuestion+";";
-	        this.st.executeUpdate(requete);
-		}
-		
-		catch (SQLException e) {
-	        
-	    }
-	}
-
-	
-	
-	
-	public void questionnairePretPourSondage (int idQuestionnaire) {
-		try {
-			String requete = "UPDATE QUESTIONNAIRE SET Etat = 'S' WHERE idQ = "+idQuestionnaire+";";
-			this.st.executeUpdate(requete);
-		}
-		
-		catch (SQLException e) {
-			
-		}
-	}
-}
-=======
 package BaseDeDonnees;
 
 import java.sql.*;
@@ -233,7 +68,7 @@ public class BDModuleConcepteur {
 	      }
 		
 		catch (SQLException e) {
-	         return -1;
+	         return 0;
 	      }
 	}
 	
@@ -261,16 +96,16 @@ public class BDModuleConcepteur {
 	
 	
 	
-	public int maxIdentifiantQuestion () {
+	public int maxIdentifiantQuestion (int identifiantQuestionnaire) {
 		try {
-	        String requete = "SELECT MAX(numQ) idMax FROM QUESTION;";
+	        String requete = "SELECT IFNULL(MAX(numQ),0) idMax FROM QUESTION WHERE idQ = "+identifiantQuestionnaire+";";
 	        ResultSet rs = this.st.executeQuery(requete);
 	        rs.next();
 	        return rs.getInt("idMax");
 	      }
 		
 		catch (SQLException e) {
-	         return -1;
+	         return 0;
 	      }
 	}
 	
@@ -316,4 +151,3 @@ public class BDModuleConcepteur {
 		}
 	}
 }
->>>>>>> 38699a40ed781a87b41066a8522b96a08f677744
