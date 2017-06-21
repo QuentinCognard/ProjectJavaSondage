aa52f046fa2a0c85fbcbe1d0fedae4deb9d8d037
Maxime Coucke <maxime.coucke@etu.univ-orleans.fr>
2017-06-22 00:50:48 +0200

    implémentation bd suppression question/questionnaire

--- src/ModuleConcepteur/ActBoutonModifQuestionnaire.java
+++ src/ModuleConcepteur/ActBoutonModifQuestionnaire.java
@@ -4,7 +4,6 @@ import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
-import javax.swing.JOptionPane;
 import BaseDeDonnees.*;
 
 public class ActBoutonModifQuestionnaire implements ActionListener {
@@ -12,16 +11,14 @@ public class ActBoutonModifQuestionnaire implements ActionListener {
 	Concepteur concepteur;
 	ModificationQuestionnaire vue;
 	Questionnaire q;
-	BDGeneral bd;
 	BDModuleConcepteur bdc;
 	
 	public ActBoutonModifQuestionnaire(Concepteur concepteur, ModificationQuestionnaire vue,
-			Questionnaire q, BDGeneral bd,BDModuleConcepteur bdc) {
+			Questionnaire q,BDModuleConcepteur bdc) {
 		super();
 		this.concepteur = concepteur;
 		this.vue = vue;
 		this.q = q;
-		this.bd = bd;
 		this.bdc = bdc;
 	}
 	
@@ -36,14 +33,6 @@ public class ActBoutonModifQuestionnaire implements ActionListener {
 					q.getTitreQuestionnaire(),q.getEtatQuestionnaire(),q.getNumClient(),
 					q.getIdentifiantUtilisateur(),q.getIdentifiantPanel()));
 			this.concepteur.afficherInfoQuestionnaire(this.q);
-		}
-		if (((JButton)arg0.getSource()).getText().equals("X")){
-			JOptionPane.showConfirmDialog(
-					((Component) concepteur.getParent()),
-				    "Voulez-vous vraiment supprimer cette question ?",
-				    "Supprimer une question",
-				    JOptionPane.YES_NO_OPTION);
-			//get la réponse
 		}
 	}
 }
